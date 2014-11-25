90848d48b2e5b83e879feb022339f0d6f69a8359
jbwexler <jbwexler@stanford.edu>
2014-11-25 09:45:13 -0800

    added voxelToRegion

--- voxel_atlas.py
+++ voxel_atlas.py
@@ -19,7 +19,6 @@ def getAtlasVoxels(region, atlas_file, dir = '/Applications/fmri_progs/fsl/data/
 	summaryimagefile = ''
 	root_header = root.find('header')
 	for images in root_header.findall('images'):
-		print images
 		if '2mm' in images.find('summaryimagefile').text:
 			summaryimagefile = images.find('summaryimagefile').text
 			
@@ -27,19 +26,43 @@ def getAtlasVoxels(region, atlas_file, dir = '/Applications/fmri_progs/fsl/data/
 	atlas=nibabel.load(os.path.join(dir, atlas_name[1:]))
 	atlas_data=atlas.get_data()
 	name_value = 0
-	data = root.find
-	
+
 	for i in range(len(root[1])):
 		name = root[1][i].text.split('(')[0].replace("'",'').rstrip(' ').lower()
+		print name
 		if name == region.lower():
 			name_value = i+1
 			voxels = numpy.where(atlas_data==name_value)
 			return voxels
-	raise ValueError('"{region}" not in "{atlas_file}"'.format(region=region, atlas_name=atlas_file))
+	raise ValueError('"{region}" not in "{atlas_file}"'.format(region=region, atlas_file=atlas_file))
        
         
-voxels = getAtlasVoxels('Corticospinal tract L','JHU-tracts.xml')
-print voxels
-for i in range(len(voxels[0])):
-    if voxels[0][i] == 120 and voxels[1][i] == 54 and voxels[2][i] == 33:
-        print "yay!"
+# voxels = getAtlasVoxels('Corticospinal tract L','JHU-tracts.xml')
+
+
+
+def voxelToRegion(X,Y,Z,atlas_file, dir = '/Applications/fmri_progs/fsl/data/atlases/'):
+	tree = ET.parse(os.path.join(dir, atlas_file))
+	root = tree.getroot()
+	summaryimagelist = []
+	summaryimagefile = ''
+	root_header = root.find('header')
+	for images in root_header.findall('images'):
+		if '2mm' in images.find('summaryimagefile').text:
+			summaryimagefile = images.find('summaryimagefile').text
+			
+	atlas_name = summaryimagefile + '.nii.gz'
+	atlas=nibabel.load(os.path.join(dir, atlas_name[1:]))
+	atlas_data=atlas.get_data()
+	atlasRegions = [x.text.lower() for x in root[1]]
+	index = atlas_data[X,Y,Z] - 1
+	print index
+	if index == -1:
+		return 'none'
+	else:
+		return atlasRegions[index]
+
+	
+print voxelToRegion(45, 16, 36,'HarvardOxford-Cortical.xml')
+	
+	
